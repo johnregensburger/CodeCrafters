411802bc4ac59f9ba8831f1fb591d234e06025b5
johnregensburger <johnregensburger2@gmail.com>
2024-03-29 13:21:00 -0500

    See Employee and TempArrays for examples

--- Employee.java
+++ Employee.java
@@ -1,3 +1,19 @@
 public class Employee {
-    
+    private final int employeeID; // auto-generated through TempArrays
+    private String lastName;
+    private String firstName;
+    private String address;
+    private int phone;
+
+    public Employee(String last, String first, String address, int phone) {
+        employeeID = 0;
+        lastName = last;
+        firstName = first;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    /*public editEmployee() {
+
+    }*/
 }
---
+++ TempArrays.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class TempArrays {
+
+    public TempArrays() {
+        ArrayList<Employee> employees; // John
+        ArrayList<Task> tasks; // Duru
+        ArrayList<Skill> skills; // Duru
+        ArrayList<Job> jobs; // Jaden
+        ArrayList<Evaluation> evaluations; // Jaden
+    }
+
+    public void editEmployee(int ID, String attribute, String newValue) {}
+    public void deleteEmployee() {}
+
+    public void addTask() {}
+    public void editTask() {}
+    public void deleteTask() {}
+
+    public void addSkill() {}
+    public void editSkill() {}
+    public void deleteSkill() {}
+
+    public void addJob() {}
+    public void editJob() {}
+    public void deleteJob() {}
+
+    public void editEvaluation() {}
+    public void deleteEvaluation() {}
+
+}
