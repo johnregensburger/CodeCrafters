c506abfc3217127b2d998dba5d6caeff49c8e234
Jmock27 <157052628+Jmock27@users.noreply.github.com>
2024-04-01 12:47:49 -0500

    i

--- Job.java
+++ Job.java
@@ -1,3 +1,18 @@
 public class Job {
-    
+    private final int employeeID;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private String supervisor;
+    private int dateHired;
+
+    public Job (String last, String first, String position, String boss, int when) {
+        employeeID = 0;
+        lastName = last;
+        firstName = first;
+        title = position;
+        supervisor = boss;
+        dateHired = when;
+        
+    }
 }
