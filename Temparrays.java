35351d2f3c858c3f0c00a9d06cb18f4d35cdf400
Jmock27 <157052628+Jmock27@users.noreply.github.com>
2024-04-01 13:01:01 -0500

    l

--- Temparrays.java
+++ Temparrays.java
@@ -22,7 +22,7 @@ public class TempArrays {
     public void deleteSkill() {}
 
     public void addJob(int ID, String name, String title, String supervisor, int dateHired) {}
-    public void editJob(String title, String supervisor, int ID) {}
+    public void editJob(String title, String supervisor, int I) {}
     public void deleteJob(String title) {}
 
     public void editEvaluation(int evaluationID, String employeeState, char grade) {}
